ad8cd6b584df6905127d2a30233121a667f5785f
Lorent Sinani <lorent.sinani0@gmail.com>
2024-01-25 13:43:08 +0100

    implement authorization logic

---
+++ truffle/contracts/Ownable.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+contract Ownable {
+    address payable _owner;
+
+    constructor() {
+        _owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(isOwner(), "You are not the owner!!");
+        _;
+    }
+
+    function isOwner() public view returns (bool) {
+        return (msg.sender == _owner);
+    }
+}
